092f7886db7c11be9708bed296091edd032dae50
Thory91 <muma5296@gmail.com>
2019-03-14 21:16:58 +0800

    Adding to github

---
+++ Assets/GameScripts/ChangeScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void reload()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void menu()
+    {
+        Time.timeScale = 1;
+        GameManager.instance.tilenum = 1;
+        SceneManager.LoadScene("Menu 3D");
+    }
+
+    public void song1()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Song1");
+    }
+
+    public void song2()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Song2");
+    }
+}
